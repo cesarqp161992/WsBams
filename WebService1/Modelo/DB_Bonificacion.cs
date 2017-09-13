5c43ed50a772ba7234e6f1671be5b82d709e6150
cesarqp161992 <cesar.quispe.pillaca@gmail.com>
2017-09-13 15:57:06 -0500

    Bonificaciones

---
+++ WebService1/Modelo/DB_Bonificacion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService1.Modelo
+{
+    public class DB_Bonificacion
+    {
+        public string FecInicio;
+        public string FecFinal;
+        public int General;
+        public string CodVnd;
+        public string Politica;
+        public string GiroNegocio;
+        public string NombrePromocion;
+        public string CodAlm;
+        public int ItemSalida;
+        public string CodSalida;
+        public int TipoUnidadSalida;
+        public int CantidadPromocion;
+        public int MaximoPedido;
+        public int GrupoCondicion;
+        public string Tipo;
+        public int Condicion;
+        public string TipoEntrada;
+        public string CodEntrada;
+        public int TipoUnidadEntrada;
+        public int CantidadCondicion;
+        public double MontoMinimo;
+        public double MontoMaximo;
+        public double Monto;
+    }
+}
